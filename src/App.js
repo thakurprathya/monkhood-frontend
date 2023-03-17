4d650182055b637d986763450031d2e32ebc9509
Prathya Thakur <98422626+thakurprathya@users.noreply.github.com>
2023-03-18 02:46:47 +0530

    added userLocation functionality depending upon which topSearch will be displayed or not

--- src/App.js
+++ src/App.js
@@ -11,25 +11,25 @@ import TopSearch from "./components/TopSearch";
 
 function App() {
     const [page, setPage] = useState('flat');
-    // const [location, setLocation] = useState({latitude: null, longitude: null});
-    const [locAccess, setLocAccess] = useState(true);
+    //eslint-disable-next-line
+    const [location, setLocation] = useState({latitude: null, longitude: null});
+    const [locAccess, setLocAccess] = useState(false);
 
     useEffect(()=>{
+        setLocAccess(false);
         document.body.classList.add('bg-[#f5f5f5]');
         document.body.classList.add('font-poppins');
         document.querySelectorAll('*[id*="fav1"] > svg > path').forEach((el) => el.classList.add('stroke-white'));
 
         //asking for user location
-        // try {
-        //     navigator.geolocation.getCurrentPosition((position) => {
-        //         console.log(position);  
-        //         setLocation({latitude: position.coords.latitude, longitude: position.coords.longitude}); 
-        //         setLocAccess(true); 
-        //     });
-        //     console.log(location, locAccess);
-        // } catch (error) {
-        //     console.log('Could not get user location:', error.message);
-        // }
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => { setLocation({latitude: position.coords.latitude, longitude: position.coords.longitude}); setLocAccess(true); },
+                (error) => { console.log(error); }
+            );
+        } else {
+            console.log('Geolocation not supported for browser');
+        }
     },[])
 
     return (
